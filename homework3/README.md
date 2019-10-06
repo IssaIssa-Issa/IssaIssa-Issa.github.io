136773b5dbc973eee58acca9c43b22dec9954b11
Issa Issa <mrforesightii@gmail.com>
2019-10-05 23:59:38 -0500

    inital upload

---
+++ homework3/README.md
@@ -0,0 +1,4 @@
+# Passward Generator
+
+This is my personal password generator. Below will be a link to the direct page along with some screen shots of what the page looks like.
+
---
+++ homework3/assets/javascript/script.js
@@ -0,0 +1,45 @@
+var genPassword = document.getElementById("generate");
+var copyText = document.getElementById("copy"); 
+var passwordField = document.getElementById("password");
+
+alert(document.getElementById("copy"));
+
+function clipboard() {
+  copyText = document.getElementById("password").textContent;
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+    
+    
+}
+
+document.getElementById("copy").addEventListener("click", clipboard);
+document.getElementById("generate").addEventListener("click", generatePassword);
+
+function generatePassword() {
+
+    userChoice  = prompt("Please choose a character length between 8 and 128.");
+      
+
+      if (userChoice > 128) {
+      
+        alert("You must pick a character count equal to or less than 128 characters!"); return false;
+      }
+      else if (userChoice < 8) {
+        alert("You must pick a character count equal to or greater than 8 characters!");
+      }
+       
+     
+  
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+      copyText = "";
+      
+      
+    for (var i = 0; i < userChoice; ++i) {
+        copyText += charset.charAt(Math.random() * i);
+    }
+document.getElementById("password").innerHTML = copyText;
+  };
+
+
+
