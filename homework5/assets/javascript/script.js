a4a44049cde07de2100105ee3a0747aad322b921
Issa Issa <mrforesightii@gmail.com>
2019-10-20 12:56:11 -0500

    Initial entry

---
+++ homework5/assets/javascript/script.js
@@ -0,0 +1,13 @@
+var moment = "https://cdn.jsdelivr.net/momentjs/2.12.0/moment.min.js";
+function moment(){
+    $.getJSON( function(moment){
+        console.log(moment);
+    });
+  
+moment();  
+}
+
+
+function updateDOM(moment) {
+
+}
