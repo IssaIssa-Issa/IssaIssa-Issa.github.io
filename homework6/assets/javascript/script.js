36e7c9e657f3282855d5c8daca4dfd4a37ed9a0b
Issa Issa <mrforesightii@gmail.com>
2019-10-28 11:08:12 -0500

    update towards html and script. able to pull city information from search bar

--- homework6/assets/javascript/script.js
+++ homework6/assets/javascript/script.js
@@ -1,28 +1,41 @@
-    $(function(){
+      $(function() {
+        $("#submit").click(function(event) {
+          event.preventDefault();
+          var name = $("#value")
+            .val()
+            .trim();
 
-        $('#submit').click(function(){
+            $(".cityPlacceHolder").html(name);
+            $(name).append($(".cityPlacceHolder"));
 
-            var name = $("#value").val();
+          if (name != "") {
+            var queryURL =
+              "https://api.openweathermap.org/data/2.5/weather?q=" +
+              name +
+              "&units=imperial" +
+              "&APPID=c1e92f1102f974ef4c31578d4a625bfa";
 
-            if (name != "") {
-
-                function weather() {
-
-                    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&units=imperial" + "&APPID=c1e92f1102f974ef4c31578d4a625bfa";
-
-                    $.ajax({
-                        type: "GET",
-                        url: queryURL,
-                    }).then(function(response){
-                        console.log(queryURL);
-                        console.log(response);
-                    });
-                }
-            }
-            else{
-                alert("Please enter a search option.")
-            }
-            weather();
+            $.ajax({
+              type: "GET",
+              url: queryURL
+            })
+              .then(function(response) {
+                console.log(queryURL);
+                console.log(response);
+                $(".card1").attr("class", "card1");
+                $(".city").html(response.name);
+                $(".temp").html(response.main.temp);
+                $(".humidity").html(response.main.humidity);
+                $(".condition").html(response.weather[0].description);
+                $("#weatherIcon").html(response.weather[0].icon);
+                $(".wind").html(response.wind.speed);
+              })
+              .fail(function(error) {
+                console.log(error);
+                alert(JSON.parse(error.responseText).message);
+              });
+          } else {
+            alert("Please enter a search option.");
+          }
         });
       });
-      
