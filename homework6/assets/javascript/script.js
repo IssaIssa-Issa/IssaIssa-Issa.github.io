f4a0003c67c88f96e8f3bd24c193b19454911401
Issa Issa <mrforesightii@gmail.com>
2019-10-28 09:49:00 -0500

    update to css html and script

--- homework6/assets/javascript/script.js
+++ homework6/assets/javascript/script.js
@@ -1,24 +1,23 @@
-    function weather() {
-
-
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&units=imperial" + "&APPID=c1e92f1102f974ef4c31578d4a625bfa";
-
-        $.ajax({
-            type: "GET",
-            url: queryURL,
-        }).then(function(response){
-            console.log(queryURL);
-            console.log(response);
-        });
-    }
-    weather();
-
     $(function(){
+
         $('#submit').click(function(){
+
             var name = $("#value").val();
 
             if (name != "") {
 
+                function weather() {
+
+                    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&units=imperial" + "&APPID=c1e92f1102f974ef4c31578d4a625bfa";
+
+                    $.ajax({
+                        type: "GET",
+                        url: queryURL,
+                    }).then(function(response){
+                        console.log(queryURL);
+                        console.log(response);
+                    });
+                }
             }
             else{
                 alert("Please enter a search option.")
@@ -26,3 +25,4 @@
             weather();
         });
       });
+      
