61da217217512cd71adcd05c70cc803555bad0da
Issa Issa <mrforesightii@gmail.com>
2020-02-17 10:43:19 -0600

    commit

--- homework9/README.md
+++ homework9/README.md
@@ -1,13 +1,11 @@
-# HTML to PDF
+# Note Taker
 
-Here you will find all of my homework assignments for the class. Each will be located inside of a folder titled which homework assignment it is.
-
-The assignment was to create a serires of prompts that take the user information and will create an HTML page that will also be converted to a PDF file.
+This is an application that will allow you to take notes and delete them later at your discression. Just type in any note and allow the application to save that note. Delete once no longer needed.
 
 ## Below is a Gif of the program being started
 
-![Here is a gif of me running the program](./images/pdf.gif)
+![Here is a gif of me running the program](./public/images/demo.gif)
 
 ## Below is a still image of what the PDF looks like after being made.
 
-[Link to homework](https://issaissa-issa.github.io/homework6)
+[Link to homework](https://issaissa-issa.github.io/homework9)
---
+++ homework9/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const savedData = fs.readFileSync("./db/db.json", "UTF-8");
+let notes;
+if (savedData) {
+    const savedNotes = JSON.parse(savedData);
+    notes = savedNotes;
+    IDNum();
+}
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "db/db.json"))
+});
+
+app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    notes.push(newNote);
+    IDNum();
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes), function (err) {
+        if (err) throw err
+    });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    const deletedNoteID = req.params.id;
+    notes.splice((deletedNoteID - 1), 1);
+    IDNum();
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes), function(err) {
+        if (err) throw err
+    });
+});
+
+//Listening at PORT 3000 
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
+function IDNum() {
+    for (i = 0; i < notes.length; i++) {
+        notes[i].id = (i + 1);
+    }
+}
